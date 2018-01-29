2cfdc13a7b3b03c833be945e4a8a67abc47ccea2
Unknown <marshallfrink@gmail.com>
2018-01-29 15:03:35 -0800

    HTML/JS
    Coding From Scratch js example

---
+++ Untitled-1.js
@@ -0,0 +1,16 @@
+function doAlert() {
+    var myFruits = ["Apples", "Oranges", "Pears", "Bannanas"];
+
+
+myFruits.forEach(function(fruit) {
+    return console.log(fruit);
+});
+}
+
+function getFirstValue() {
+    return parseInt(doucument.getElementById("firstValue").vaule);
+}
+
+function getSecondValue() {
+    return parseInt(doucument.getElementById("SecondValue").vaule);
+}
