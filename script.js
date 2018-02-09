316b7b6c0d78e1dfec9c5dd4848a826eb581d0eb
Marshall <marshallfrink@gmail.com>
2018-02-08 16:58:21 -0800

    Completed HTML

---
+++ script.js
@@ -0,0 +1,4 @@
+function idCard () {
+    var firstName = document.getElementById("FirstName")
+    var lastName = document.getElementById("LastName")
+    var address = document.getElementById("Address")
