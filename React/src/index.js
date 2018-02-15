7448ba893d8c5d9ee3ae2721758c0207d953e1e7
Marshall <marshallfrink@gmail.com>
2018-02-15 10:36:14 -0800

    17111
    Really hard to do

--- React/src/index.js
+++ React/src/index.js
@@ -1,1 +1,16 @@
-alert("it's alive");
+const arr = [
+    {"name":"chevy", "count": 2},
+    {"name":"ford", "count": 5},
+    {"name":"acura", "count": 3},
+    {"name":"honda", "count": 16},
+];
+
+const newArr = [];
+
+for(let i= 0, l = arr.length; i< l; i++){
+    if(arr[i].name === "ford" ){
+		newArr.push(arr[i]);
+	}
+}
+
+console.log("Filter results:",newArr);
